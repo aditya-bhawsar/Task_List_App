15ef42b3b5f29551e8fc271a99ff951a992dd57b
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2021-05-17 21:31:17 +0530

    Dependency Update

--- app/src/main/java/com/aditya/to_do/viewModel/ListingViewModel.kt
+++ app/src/main/java/com/aditya/to_do/viewModel/ListingViewModel.kt
@@ -1,16 +1,18 @@
 package com.aditya.to_do.viewModel
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aditya.to_do.model.TaskModel
 import com.aditya.to_do.repository.AppRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ListingViewModel @ViewModelInject constructor(private val appRepo: AppRepository): ViewModel() {
+@HiltViewModel
+class ListingViewModel @Inject constructor(private val appRepo: AppRepository): ViewModel() {
 
     val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(false)
     fun checkIfDatabaseEmpty(boolean: Boolean){ emptyDatabase.value = boolean }
--- app/src/main/java/com/aditya/to_do/viewModel/NewViewModel.kt
+++ app/src/main/java/com/aditya/to_do/viewModel/NewViewModel.kt
@@ -1,14 +1,16 @@
 package com.aditya.to_do.viewModel
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aditya.to_do.model.TaskModel
 import com.aditya.to_do.repository.AppRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NewViewModel @ViewModelInject constructor(private val appRepo: AppRepository): ViewModel() {
+@HiltViewModel
+class NewViewModel @Inject constructor(private val appRepo: AppRepository): ViewModel() {
 
     fun insertData(task: TaskModel) = viewModelScope.launch(Dispatchers.IO) { appRepo.insertData(task) }
 
